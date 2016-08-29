86ada43f46556df680708ed6e6d979506bc79343
Ole <ole@vprsm.de>
2016-08-29 17:42:43 +0200

    tests: Change example numbers for Buffer get/set

--- tests/utils/buffer.cpp
+++ tests/utils/buffer.cpp
@@ -6,10 +6,10 @@ using namespace knxproto;
 TEST_CASE("put/get") {
 	Buffer buf(15);
 
-	uint8_t a1 = 120, a2;
-	uint16_t b1 = 30000, b2;
-	uint32_t c1 = 1200000, c2;
-	uint64_t d1 = 300000000000, d2;
+	uint8_t a1 = 0xAB, a2;
+	uint16_t b1 = 0xCDEF, b2;
+	uint32_t c1 = 0xABCDEF98, c2;
+	uint64_t d1 = 0x76543210FEDCBA89, d2;
 
 	REQUIRE(put(buf, a1, b1, c1, d1));
 	REQUIRE(get(buf, a2, b2, c2, d2));
